64b6773eb0a3bb1fda83345d6f8d2e435e9dd6cf
Oluwafemi Popoola <oluwafemipopoola00@gmail.com>
2025-07-28 11:47:52 +0100

    Update change_over_time_analysis.sql

--- scripts/change_over_time_analysis.sql
+++ scripts/change_over_time_analysis.sql
@@ -1,1 +1,46 @@
+/*
+===============================================================================
+Time Series Exploration – Yearly and Monthly Sales Trends
+===============================================================================
+Purpose:
+    - To analyze sales trends over time by aggregating sales amounts.
+    - To support trend analysis for reporting and dashboarding (e.g., year-over-year or month-over-month performance).
 
+SQL Functions Used:
+    - YEAR(), DATETRUNC(), SUM(), GROUP BY, ORDER BY
+
+Tables Involved:
+    - gold.fact_sales
+===============================================================================
+*/
+
+-- =============================
+-- Yearly Sales Trend Analysis
+-- =============================
+SELECT 
+    YEAR(order_date) AS yearly_basis,        -- Extracts the year from the order date
+    SUM(sales_amount) AS total_sales         -- Aggregates total sales for each year
+FROM 
+    gold.fact_sales
+WHERE 
+    YEAR(order_date) IS NOT NULL             -- Filters out rows with null year
+GROUP BY 
+    YEAR(order_date)                         -- Groups data by year
+ORDER BY 
+    YEAR(order_date);                        -- Orders results chronologically
+
+
+-- ==============================
+-- Monthly Sales Trend Analysis
+-- ==============================
+SELECT 
+    DATETRUNC(MONTH, order_date) AS monthly_basis,  -- Truncates order date to the month level
+    SUM(sales_amount) AS total_sales                -- Aggregates total sales for each month
+FROM 
+    gold.fact_sales
+WHERE 
+    DATETRUNC(MONTH, order_date) IS NOT NULL        -- Filters out rows with null month
+GROUP BY 
+    DATETRUNC(MONTH, order_date)                    -- Groups data by month
+ORDER BY 
+    DATETRUNC(MONTH, order_date);                   -- Orders results chronologically
